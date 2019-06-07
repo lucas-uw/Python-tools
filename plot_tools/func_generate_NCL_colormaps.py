499083d5cb98d524a4083379084e8d0d407f5509
Xiaodong Chen <xiaodc.work@gmail.com>
2019-06-07 13:37:24 -0700

    added NCL colormap functions

---
+++ plot_tools/func_generate_NCL_colormaps.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib
+
+import colormath, colormath.color_objects, colormath.color_conversions
+from colormath.color_objects import sRGBColor
+
+import urllib
+import re
+
+color_obj_dict = {'sRGB':colormath.color_objects.sRGBColor,
+                  'HSV':colormath.color_objects.HSVColor,
+                  'Lab':colormath.color_objects.LabColor,
+                  'LCHuv':colormath.color_objects.LCHuvColor,
+                  'LCHab':colormath.color_objects.LCHabColor,
+                  'XYZ':colormath.color_objects.XYZColor}
+
+def __rgb_to_array(rgb_color):
+    r = np.minimum(1, round(rgb_color.rgb_r*10000)/10000)
+    g = np.minimum(1, round(rgb_color.rgb_g*10000)/10000)
+    b = np.minimum(1, round(rgb_color.rgb_b*10000)/10000)
+    return r,g,b
+
+
+def create_palette(start_rgb, end_rgb, n, colorspace):
+    # convert start and end to a point in the given colorspace
+    start = colormath.color_conversions.convert_color(start_rgb, colorspace).get_value_tuple()
+    end = colormath.color_conversions.convert_color(end_rgb, colorspace).get_value_tuple()
+
+    # create a set of n points along start to end
+    points = list(zip(*[np.linspace(start[i], end[i], n) for i in range(3)]))
+
+    # create a color for each point and convert back to rgb
+    rgb_colors = [colormath.color_conversions.convert_color(colorspace(*point), sRGBColor) for point in points]
+
+    # finally convert rgb colors back to hex
+    return [__rgb_to_array(color) for color in  rgb_colors]
+
+
+def __retrive_NCL_webcontent(cmapname):
+    target_url = 'https://www.ncl.ucar.edu/Document/Graphics/ColorTables/Files/%s.rgb' % cmapname
+    request = urllib.request.urlopen(target_url)
+    return request
+
+
+def __collect_discrete_NCL_cmap(cmapname):
+    rawdata = __retrive_NCL_webcontent(cmapname)
+
+    cmap_color_list = list()
+
+    color_section_sig = 0
+
+    for line in rawdata:
+
+        line_decode = line.decode('utf-8')
+        info = re.split('\s+', line_decode.replace('\n',''))
+
+        if color_section_sig==1:
+            if info[0]=='' and len(info)>=3:
+                cmap_color_list.append((int(info[1])/255, int(info[2])/255, int(info[3])/255))
+
+        if info[0]=='ncolors':
+            color_section_sig = 1  # meaning now we are at color lines (or "r g b" line)
+
+    return cmap_color_list
+
+
+def __cmap_refinement(raw_cmap_rgb, n_interpolate=10, workspace=color_obj_dict['sRGB']):
+    # workspace:  choose which color space the refinement is conducted.
+    #             refer to https://stackoverflow.com/questions/55032648/given-a-start-color-and-a-middle-color-how-to-get-the-remaining-colors-python
+
+    n_in = len(raw_cmap_rgb)
+
+    new_array = list()
+
+    for i in np.arange(n_in-1):
+        out_colors = create_palette(sRGBColor(*raw_cmap_rgb[i], is_upscaled=False), sRGBColor(*raw_cmap_rgb[i+1], is_upscaled=False), n_interpolate+1, workspace)
+        for j in np.arange(len(out_colors)-1):
+            new_array.append(out_colors[j])
+
+    return new_array
+
+
+def generate_NCL_cmap(cmapname, cont_opt=False, cont_param_n=10, cont_param_ws='sRGB',
+                      white_first=False):
+    # description:
+    #     main function to call.
+    #     cmapname:      taken as shown on the NCL website
+    #     cont_opt:      to convert the discreate colormap to continuous colormap
+    #     cont_param_n:  how many "intermediate" colors to be inserted to the nearby discreate colors
+    #     cont_param_ws: color space to conduct interploation. Default to "sRGB", which should work for most cases
+    #     white_first:   whether to set the first color as white. May be useful if the minimum does not mean anything
+
+    cmap_discrete = __collect_discrete_NCL_cmap(cmapname)
+    if white_first==True:
+        cmap_discrete[0] = (1,1,1)
+
+    if cont_opt==False:
+        out_cmap = cmap_discrete
+
+    if cont_opt==True:
+        out_cmap = __cmap_refinement(cmap_discrete, n_interpolate=cont_param_n, workspace=color_obj_dict[cont_param_ws])
+
+    return matplotlib.colors.ListedColormap(out_cmap)
